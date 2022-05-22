f146a1be8ed46c26dc21f446516c2011daa18f83
huukhuong <huukhuong.it@gmail.com>
2022-05-22 17:58:46 +0700

    delete all file in branch main

--- README.md
+++
@@ -1,4 +0,0 @@
-# TKB_SGU
-### Hello,
-#### When I started working on this project, only God and I knew how to run it.
-#### Now, only God knows it!
--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('HOST', 'localhost');
-    define('USERNAME', 'huukhuon_root');
-    define('PASSWORD', '25102001k');
-    define('DATABASE', 'huukhuon_tkb');
-
-    $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
-    mysqli_set_charset($conn, "utf8");
