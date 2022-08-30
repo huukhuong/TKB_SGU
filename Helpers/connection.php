f41854f9d0101d4b945ce55f6bf97d76220d78ca
Võ Hoàng Kiệt <75366637+thekids1002@users.noreply.github.com>
2022-08-30 17:17:27 +0700

    Delete connection.php

--- Helpers/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('HOST', 'localhost');
-    define('USERNAME', 'lhkkarrn_root');
-    define('PASSWORD', '25102001k');
-    define('DATABASE', 'lhkkarrn_tkb');
-
-    $conn = mysqli_connect(HOST, USERNAME, PASSWORD, DATABASE);
-    mysqli_set_charset($conn, "utf8");
